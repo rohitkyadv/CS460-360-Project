3e446ededa441ba574e78c0c88911cd4bb8529f3
Nick <kren5141@vandals.uidaho.edu>
2017-10-06 05:49:54 +0000

    More documentation & comments

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # Database_Systems_Project
 This is the course project for Uidaho cs 360
 
+http://www.nltk.org/install.html
+* Install NLTK: run `sudo pip install -U nltk`
+* Install Numpy (optional): `run sudo pip install -U numpy`
+* Test installation: run python then type `import nltk`
+
+http://www.nltk.org/data.html
+* To download all data `python -c "import nltk; nltk.download(all)"`
+* To download only parts `python` then `import nltk` then `nltk.download()`
+
 
 # References & tools
 Voice recognition
@@ -11,3 +20,6 @@ https://html-online.com/editor/
 
 Natural language tookkit
 http://www.nltk.org/
+
+Other voice recognition alternatives
+https://stackoverflow.com/a/6351055
--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python
-import sys
-from datetime import datetime
-import nltk
+from sys import argv                # for arguments
+from datetime import datetime       # for date printing
+import nltk                         # natural language tookkit
+
 print "Time is: ", datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+ # we use <br> for new lines since this is generating html content for php
 print "<br>"
-#print "\nArg1: ", sys.argv[1:]
+
+#print "\nArg1: ", argv[1:]  # Will print out all arguments starting at 1
 #print "<br>"
 
-sentence = sys.argv[1]
+# echo the sentence
+sentence = argv[1]
+#sentence = """At eight o'clock on Thursday morning"""   # debugging
 print "Sentence is: %s" % sentence
 print "<br>"
-#sentence = """At eight o'clock on Thursday morning"""
+
+# separates sentence into tokens or individual words
 tokens = nltk.word_tokenize(sentence)
 print ("\nTokens %r\n" % tokens)
 print "<br>"
+
+# identifies each token to a part of speech
 tagged = nltk.pos_tag(tokens)
 print ("Tagged words: %r\n" % tagged[0:6])
