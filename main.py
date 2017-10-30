20838782dfc1f64db069dfd71dba051f840c5410
Ubuntu <ubuntu@ip-172-31-82-135.ec2.internal>
2017-10-30 19:19:53 +0000

    Spelling fix

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 from sys import argv                # for arguments
 from datetime import datetime       # for date printing
 import nltk                         # natural language tookkit
 
-print "Time is: ", datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+print ("Time is: ", datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
  # we use <br> for new lines since this is generating html content for php
-print "<br>"
+print ("<br>")
 
 #print "\nArg1: ", argv[1:]  # Will print out all arguments starting at 1
 #print "<br>"
@@ -13,13 +13,13 @@ print "<br>"
 # echo the sentence
 sentence = argv[1]
 #sentence = """At eight o'clock on Thursday morning"""   # debugging
-print "Sentence is: %s" % sentence
-print "<br>"
+print ("Sentence is: %s" % sentence)
+print ("<br>")
 
 # separates sentence into tokens or individual words
 tokens = nltk.word_tokenize(sentence)
 print ("\nTokens %r\n" % tokens)
-print "<br>"
+print ("<br>")
 
 # identifies each token to a part of speech
 tagged = nltk.pos_tag(tokens)
