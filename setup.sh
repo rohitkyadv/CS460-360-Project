efdf7fa0e7333e03b39c7333fe97304a84772469
Nick <kren5141@vandals.uidaho.edu>
2017-11-13 09:05:00 +0000

    Updated setup script & Readme file

--- setup.sh
+++ setup.sh
@@ -1,41 +1,60 @@
 #!/bin/bash
 
-echo "Installing webserver and other binaries"
-sudo apt update -qq
-sudo apt install -y git htop nginx ufw
+f_install_server() {
+    echo "Installing webserver and other binaries"
+    sudo apt update -qq
+    sudo apt install -y git htop nginx ufw
+    
+    # install php7.1
+    sudo apt-get install software-properties-common
+    sudo add-apt-repository -y ppa:ondrej/php
+    sudo apt-get update -qq
+    sudo apt install -y php7.1 php7.1-fpm php7.1-mysql
+    
+    echo "Downloading adminer"
+    wget "https://www.adminer.org/latest.php"
+    mv -v latest.php adminer.php
+    
+    echo "Setting up firewall"
+    ufw default deny incomming
+    ufw default allow outgoing
+    ufw allow ssh
+    ufw allow http
+    ufw allow https
+    ufw allow mysql
+    ufw enable
+    ufw status
+}
 
-# install php7.1
-sudo apt-get install software-properties-common
-sudo add-apt-repository -y ppa:ondrej/php
-sudo apt-get update -qq
-sudo apt install -y php7.1 php7.1-fpm php7.1-mysql
-
-echo "Installing python and dependancies"
-sudo apt install -y python3 python3-pip
-pip3 install --upgrade nltk numpy pymysql
-
-echo "Configuring python nltk"
-python3 -m nltk.downloader all
-
-echo "Setting up project"
-cd ~
-git clone https://github.com/Cyberzoid1/Database_Systems_Project.git
-cd Database_Systems_Project
-git fetch --all --prune
-git pull
-
-echo "Downloading adminer"
-wget "https://www.adminer.org/latest.php"
-mv -v latest.php adminer.php
+f_install_pythonApp() {
+    echo "Installing python and dependancies"
+    sudo apt update -qq
+    sudo apt install -y python3 python3-pip
+    pip3 install --upgrade nltk numpy pymysql
+    
+    echo "Configuring python nltk"
+    python3 -m nltk.downloader all
+}
 
 
-echo "Setting up firewall"
-ufw default deny incomming
-ufw default allow outgoing
-ufw allow ssh
-ufw allow http
-ufw allow https
-ufw allow mysql
-ufw enable
-ufw status
+echo "Choose setup option
+1 - Run server setup
+2 - Run python application setup
+3 - Run both"
 
+while true; do
+  read -p "> " input
+  case $input in
+      [1]* ) 
+        f_install_server
+        break;;
+      [2]* ) 
+        f_install_pythonApp
+        break;;
+      [3]* ) 
+        f_install_server
+        f_install_pythonApp
+        break;;
+      * ) echo "Please chose 1, 2 or 3.";;
+  esac
+done
