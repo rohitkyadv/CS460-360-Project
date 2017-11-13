a400fa8806c438871a71ef83e2131d2541f5612e
Nick Krenowicz <kren5141@vandals.uidaho.edu>
2017-11-13 06:21:15 +0000

    tree intro

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
+import matplotlib
+# Force matplotlib to not use any Xwindows backend.
+matplotlib.use('Agg')
+
+
 from sys import argv                # for arguments
 from datetime import datetime       # for date printing
 import nltk                         # natural language tookkit
 
 print ("Time is: ", datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 # print current time to file to prove we ran
+
+# Test if argv[1] is not null
+if len(argv) < 2:
+    print("A sentence must be included")
+    exit()
+
 file = open("log.log", "a")
 file.write("Time is: %s - %s\n" % (datetime.now(),  argv[1]))
 file.close()
@@ -28,4 +39,38 @@ print ("<br>")
 
 # identifies each token to a part of speech
 tagged = nltk.pos_tag(tokens)
-print ("Tagged words: %r\n" % tagged[0:6])
+print ("Tagged words: %r\n" % tagged)
+
+
+
+print ("<br>")
+print ("tag1: ", tagged[1])
+#print ("tag2: ", tagged[2])
+#print ("tag3: ", tagged[3])
+#print ("tag4: ", tagged[4])
+#print ("tag5: ", tagged[5])
+
+from nltk.corpus import treebank
+t = treebank.parsed_sents('wsj_0001.mrg')[0]
+#t.draw()
+
+
+from nltk import Tree
+
+
+
+
+dp1 = Tree('dp', [Tree('d', ['the']), Tree('np', ['dog'])])
+dp2 = Tree('dp', [Tree('d', ['the']), Tree('np', ['cat'])])
+vp = Tree('vp', [Tree('v', ['chased']), dp2])
+tree = Tree('s', [dp1, vp])
+print(tree)
+
+print(tree.pformat_latex_qtree())
+tree.pretty_print()
+
+
+sent = ['I', 'shot', 'an', 'elephant', 'in', 'my', 'pajamas']
+parser = nltk.ChartParser(groucho_grammar)
+for tree in parser.parse(sent):
+    print(tree)
