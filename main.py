5001089e46943ca413bb1631b72c0eb14ff4ad9f
Nick <kren5141@vandals.uidaho.edu>
2017-12-04 02:58:04 +0000

    Added schema lookup dable and attribute finding

--- main.py
+++ main.py
@@ -63,20 +63,81 @@ def db_run_querey(db, query):
         cur.close()
         conn.close()
     return rtn_list
-        
-db = "drugsdatabase"
-tables = db_run_querey(db, "SELECT table_name FROM information_schema.tables where table_schema='drugsdatabase';")
-for i in tables:
-    print (i)
-    db_run_querey('information_schema', "SELECT column_name FROM `COLUMNS` where table_schema = '" + db + "' and table_name = '" + i + "'")
-print ("------")
+    
 
-
+# SQL reference queries
 # SELECT TABLE_SCHEMA, TABLE_NAME FROM information_schema.tables where table_schema='drugsdatabase';
 # SELECT table_name, column_name FROM `COLUMNS` where table_schema = 'drugsdatabase' and table_name = 'marketingstatus'
 # SELECT table_name, column_name FROM `COLUMNS` where table_schema = 'drugsdatabase'
 
+# SELECT
+# FROM
+# WHERE
+sql_output = {}
+sql_output['SELECT'] = ""
+sql_output['FROM']   = ""
+sql_output['WHERE']  = ""
+        
+#db = "drugsdatabase"
+db = "world"
+tables = db_run_querey(db, "SELECT table_name FROM information_schema.tables where table_schema='" + db + "';")
+print ("--------------------")
 
+db_schema2 = {}
+for t in tables:
+    db_schema2[t] = {}  # create dictionary of attributes
+    #print (i)
+    attributes = db_run_querey('information_schema', "SELECT column_name FROM `COLUMNS` where table_schema = '" + db + "' and table_name = '" + t + "'")
+    for a in attributes:
+        db_schema2[t][a] = a
+print ("------")
+#print("Post table grab: %s" % db_schema2)       # debugger
+
+
+# depending on how the disctionary is sorted this may return different results
+# stops on first hit. Problem if multiple tables use the same attribute
+def find_attribute(keyword, schema):
+    # is keyword a table?
+    if keyword in schema:
+        print("%s is a table name" % keyword)
+    
+    # is keyword a attribute of a table
+    match_found = False
+    rtn_list = []
+    for t in schema:    # loop through tables
+        #print ("table: %s" % t)                                             # debugger
+        for a in schema[t]:     # search attributes in table
+            #print ("attribute: %s" % a)                                     # debugger
+            if keyword == a:
+                print("Found match for %s in table %s" % (keyword, t))      # debugger
+                match_found = True
+                rtn_list.append((t,a))
+    if not match_found:
+        print ("No match found for %s" % keyword)
+        return 0
+    #print(rtn_list)
+    return rtn_list
+
+g = find_attribute("Capital", db_schema2)
+print (g)
+
+find_attribute("Language", db_schema2)
+find_attribute("Population", db_schema2)
+
+find_attribute("Strength", db_schema2)
+
+
+
+exit()  # not ready yet
+# Run output SQL query
+db_run_querey(db, sql_output['SELECT'] + sql_output['FROM'] + sql_output['WHERE'])
+
+
+
+
+
+
+exit()
 db_schema = { 
             "Table1" :
               {
@@ -84,6 +145,17 @@ db_schema = {
                   "c2": ["c2", "a1", "a2"],
                   "c3": ["c3"],
                   "c4": ["c4"]
+              },
+            "Table2" : ["c1", "a1", "a2"]
+            }
+
+db_schema_curr = { 
+            "Table1" :
+              {
+                  "Movies": "Movies",
+                  "c2": "c2",
+                  "c3": "c3",
+                  "c4": "c4"
               },
             "Table2" : ["c1", "a1", "a2"]
             }
