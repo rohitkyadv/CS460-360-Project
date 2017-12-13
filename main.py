c532edd28c6cb96d2a31198012a20be266f80496
Nick <kren5141@vandals.uidaho.edu>
2017-12-13 18:44:01 +0000

    Initial traversal tree code. Code rearrangement. Removed sql code moved to lib_sql_db file

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import nltk                         # natural language tookkit
 from stat_parser import Parser, display_tree    # https://stackoverflow.com/a/17935542
 from nltk.corpus import treebank
 from nltk import Tree
-import pymysql
 from lib_alias import alias_lookup
+from lib_sql_db import db_run_querey, find_attribute
 
 # print current time to file to prove we ran
 print (datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
@@ -44,104 +44,6 @@ print("\nTree 2: ", tree.pformat_latex_qtree())
 print("\nPretty tree:\n")
 tree.pretty_print()
 
-if False:
-    print ("--- Looking through the tree -----")
-    print (tree[1])
-    print (type(tree[1]))
-    x = tree[1][1]
-    print ("val: %s, type: %s" % (x, type(x)))
-    print (len(x))
-    print (len(tree))
-    print (tree.label())
-    print (tree[0].label())
-    print (tree[0][0].label())
-    
-    print (len(tree[0][0]))
-    print (len(tree[0][0][0]))
-    print (tree[0][0][0])
-    
-    print(tree[0].leaves())
-    print(tree[1].leaves())
-
-
-
-# this runs a database querey and returns a list of what was returned.
-def db_run_querey(db, query):
-    # https://stackoverflow.com/a/13846183
-    config = {
-      'user': 'nick',
-      'passwd': 'harvey',
-      'host': 'database.nkren.net',
-      'db': db,
-    }
-    try:
-      conn = pymysql.connect(**config)
-    except Exception as e:
-        print ("error ", e)
-        return 1
-    else:
-        cur = conn.cursor()
-        try:
-            cur.execute(query)
-        except Exception as e:
-            print("An error occured when executing query")
-            print("\tThe query: %r " % query)
-            print (e)
-            return 1
-        else:
-            rtn_list = []
-            #print (cur.fetchall())
-            for response in cur:
-                #print("Response: %r\tType: %r" % (response, type(response)))   # response is a tuple
-                print(response)
-                rtn_list.append(response[0])
-            #print ("rtn_list: %r" % rtn_list)
-        cur.close()
-        conn.close()
-    return rtn_list
-
-
-# this function takes a keyword and finds that keyword in the database schema.
-# if a hit is found, this function returns a list of tuples for each match [(table, column), (table, column)]
-def find_attribute(keyword, schema):
-    keyword = keyword.lower()
-    # is keyword a table?
-    #if keyword in schema:
-    #    print("%s is a table name" % keyword)
-    
-    # is keyword a attribute of a table
-    match_found = False
-    rtn_list = []
-    for t in schema:                            # loop through tables
-        #print ("table: %s" % t)                # debugger
-        for a in schema[t]:                     # search attributes in table
-            #print ("  attribute: %s" % a)      # debugger
-            if keyword in schema[t][a]:         # test if keyword exits in the list of aliases for a given attribute
-                #print("Found match for %s in table %s, column: %s" % (keyword, t, a))      # debugger
-                match_found = True
-                rtn_list.append((t,a))          # add result tuple to return list
-    if not match_found:
-        print ("No match found for %s" % keyword)
-        return 0
-    #print("\n>>" + keyword + ": " + str(rtn_list))
-    return rtn_list
-
-# SQL reference queries
-# SELECT TABLE_SCHEMA, TABLE_NAME FROM information_schema.tables where table_schema='drugsdatabase';
-# SELECT table_name, column_name FROM `COLUMNS` where table_schema = 'drugsdatabase' and table_name = 'marketingstatus'
-# SELECT table_name, column_name FROM `COLUMNS` where table_schema = 'drugsdatabase'
-
-
-
-
-
-# SELECT
-# FROM
-# WHERE
-sql_output = {}
-sql_output['SELECT'] = ""
-sql_output['FROM']   = ""
-sql_output['WHERE']  = ""
 
 
 # build a schema of the database
@@ -160,7 +62,7 @@ for t in tables:
     for a in attributes:
         db_schema[t][a.lower()] = [a.lower()]
 print ("------")
-#print("Post table grab: %s" % db_schema)       # debugger
+#print("Post table grab: %s" % db_schema)               # debugger
 db_schema = alias_lookup(db_schema)
 #print("-----\nPost table alias: %s" % db_schema)       # debugger
 
@@ -184,11 +86,55 @@ g = find_attribute("Strength", db_schema)
 print ("Strength  %s"%g)
 
 
+
+if False:
+    print ("--- Looking through the tree -----")
+    print (tree[1])
+    print (type(tree[1]))
+    x = tree[1][1]
+    print ("val: %s, type: %s" % (x, type(x)))
+    print (len(x))
+    print (len(tree))
+    print (tree.label())
+    print (tree[0].label())
+    print (tree[0][0].label())
+    
+    print (len(tree[0][0]))
+    print (len(tree[0][0][0]))
+    print (tree[0][0][0])
+    
+    print(tree[0].leaves())
+    print(tree[1].leaves())
+    
+    
+def traverseTree(tree):
+    tree_len = len(tree)
+    tree_height = tree.height()
+    print ("Lable: %s    Len: %d   Height: %d    Leaves: %s" % (tree.label(), tree_len, tree.height(), tree.leaves()))
+    tree.pretty_print()
+    for i in range(tree_len):
+        print ("i: ", i)
+        if tree_height >2:
+            traverseTree(tree[i])
+        # height 2 is pos, height 1 is the word
+        
+# SELECT
+# FROM
+# WHERE
+sql_output = {}
+sql_output['SELECT'] = ""
+sql_output['FROM']   = ""
+sql_output['WHERE']  = ""
+
+print ("--- Traversing the tree ---")
+traverseTree(tree)
+
+
+
+
 exit()  # not ready yet
 # Run output SQL query
 db_run_querey(db, sql_output['SELECT'] + sql_output['FROM'] + sql_output['WHERE'])
-
-
 
 
 
